79151db0955341bb0769bc535ff96caec84a35c4
DmytroZhuk <dm.zhuk@gmail.com>
2025-01-22 00:14:28 +0200

    Added vaiables.js, RegistrationScreenStyles.js,

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,21 +1,13 @@
-import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  FlatList,
-  Button,
-  Image,
-} from "react-native";
-import { useCallback } from "react";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
+import { useState } from "react";
 import { useFonts } from "expo-font";
-import { StatusBar } from "expo-status-bar";
+import RegistrationScreen from "./src/screens/Registration/RegistrationScreen";
+import LogInScreen from "./src/screens/LogIn/LogInScreen";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
     "Roboto-Light": require("./assets/fonts/Roboto-Light.ttf"),
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
     "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
     "Roboto-SemiBold": require("./assets/fonts/Roboto-SemiBold.ttf"),
     "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
@@ -23,91 +15,33 @@ export default function App() {
   });
 
   if (!fontsLoaded) {
-    return null;
+    return (
+      <View style={styles.loader}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
   }
 
+  const [isLogin, setIsLogin] = useState(false);
+
+  const goToLogin = () => setIsLogin(true);
+  const goToRegistration = () => setIsLogin(false);
+
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <>
-        <Image
-          source={require("../AwesomeProject/images/Bg.png")}
-          resizeMode="cover"
-          style={styles.image}
-        />
-      </>
-      <Text style={styles.text}>
-        Awesome! My first React Native Screen finally there!
-      </Text>
-      <Button title="Navigate" onPress={() => console.log("Button pressed!")} />
-    </View>
+    <>
+      {isLogin ? (
+        <LogInScreen goToRegistration={goToRegistration} />
+      ) : (
+        <RegistrationScreen goToLogin={goToLogin} />
+      )}
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  loader: {
     flex: 1,
-    padding: 8,
-    backgroundColor: "#eaeaea",
     alignItems: "center",
     justifyContent: "center",
   },
-  text: {
-    fontFamily: "Roboto-SemiBold",
-    fontSize: 20,
-    color: "#fff",
-    padding: 24,
-  },
-  title: {
-    marginTop: 16,
-    paddingVertical: 8,
-    borderWidth: 4,
-    borderColor: "#20232a",
-    borderRadius: 6,
-    backgroundColor: "#61dafb",
-    color: "#20232a",
-    textAlign: "center",
-    fontSize: 30,
-    fontWeight: "bold",
-  },
-  image: {
-    position: "absolute",
-    top: 0,
-    bottom: 0,
-    height: "100%",
-    width: "100%",
-  },
 });
-
-/* const COURSES = [
-  {
-    id: "45k6-j54k-4jth",
-    title: "HTML",
-  },
-  {
-    id: "4116-jfk5-43rh",
-    title: "JavaScript",
-  },
-  {
-    id: "4d16-5tt5-4j55",
-    title: "React",
-  },
-  {
-    id: "LG16-ant5-0J25",
-    title: "React Native",
-  },
-];
-
-export default function App() {
-  const [courses, setCourses] = useState(COURSES);
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={courses}
-        renderItem={({ item }) => <Text>{item.title}</Text>}
-        keyExtractor={(item) => item.id}
-      />
-    </SafeAreaView>
-  );
-} */
